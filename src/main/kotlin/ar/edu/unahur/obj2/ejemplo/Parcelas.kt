88501a56bfef24d0bfcbb2a22f6d85581ffbd030
CristianLuna88 <cristiansebastian.luna@estudiantes.unahur.edu.ar>
2022-04-16 17:56:34 -0300

    se agrego INTA y se modificaron Parcela y Planta

--- src/main/kotlin/ar/edu/unahur/obj2/ejemplo/Parcelas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/ejemplo/Parcelas.kt
@@ -1,22 +1,38 @@
 package ar.edu.unahur.obj2.ejemplo
 
-class Parcela(var ancho: Int, var largo: Int, var horasDeSol: Int, var listPlantas : MutableList<Planta>){
-    fun superficie(): Int{
-        return ancho * largo
-    }
-    fun agregarPlanta(unaPlanta: Planta) {
-        listPlantas.add(unaPlanta)
-    }
-    fun cantidadMaximaTolerada() = if (ancho> largo) {superficie()/5} else {superficie()/3 + largo}
-    fun tieneComplicaciones() = listPlantas.any{n:Planta -> n.horasDeSolTolera() > horasDeSol}
-    fun plantar(unaPlanta: Planta){
-        if (cantidadMaximaTolerada() < listPlantas.size || unaPlanta.horasDeSolTolera() < horasDeSol+2){
-            agregarPlanta(unaPlanta)
+import kotlin.math.ceil
+
+open class Parcela(var ancho: Double, var largo: Double, val horasDeSol: Int, val listaDePlantas: MutableList<Planta>) {
+    fun superficie() = ancho * largo
+
+    fun cantMaxDePlantas() = ceil(if(ancho > largo) {superficie()/5} else {superficie()/3 + largo})
+
+    fun cantDePlantasEnParcela() = listaDePlantas.size.toDouble()
+
+    fun plantarSinCondicion(unaPlanta: Planta) = listaDePlantas.add(unaPlanta)
+
+    fun plantarUnaPlanta(unaPlanta: Planta) =
+        if (condicionParaPlantar(unaPlanta)){
+            listaDePlantas.add(unaPlanta)
+        }else{
+            error("NO CUMPLE LA CONDICION PARA PLANTAR")
         }
-        else{
-           throw Exception("Se supero la cantidad de Plantas o le falta Sol")
-        }
-    }
+
+    fun quitar(unaPlanta: Planta) = listaDePlantas.remove(unaPlanta)
+
+    fun condicionParaPlantar(unaPlanta: Planta) = cantMaxDePlantas() < cantDePlantasEnParcela() || horasDeSol > unaPlanta.horasDeSolToleradas()+2
+
+    fun tieneComplicaciones() = listaDePlantas.any{ it.horasDeSolToleradas() > horasDeSol}
+
+    open fun seAsociaBienA(unaPlanta: Planta) = false
+
+    fun porcentajeBienAsociada() = listaDePlantas.count{n ->this.seAsociaBienA(n)} / listaDePlantas.size * 100.0
+
+}
+class ParcelaEcologica(ancho: Double, largo: Double, horasDeSol: Int, listaDePlantas: MutableList<Planta>) : Parcela(ancho, largo,horasDeSol,listaDePlantas){
+    override fun seAsociaBienA(unaPlanta: Planta) = !tieneComplicaciones() && unaPlanta.esIdeal(this)
 }
 
-
+class ParcelaIndustrial(ancho: Double, largo: Double, horasDeSol: Int, listaDePlantas: MutableList<Planta>) : Parcela(ancho, largo,horasDeSol,listaDePlantas){
+    override fun seAsociaBienA(unaPlanta: Planta) = listaDePlantas.size < 2 && unaPlanta.esFuerte()
+}
