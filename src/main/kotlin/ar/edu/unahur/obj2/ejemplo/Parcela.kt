ed1521fa962a3e45a952cc941faede23948ee884
CristianLuna88 <cristiansebastian.luna@estudiantes.unahur.edu.ar>
2022-04-26 22:05:24 -0300

    se modifico en Parcela el metodo seAsociaBien() y se agrego cambiarTipo

--- src/main/kotlin/ar/edu/unahur/obj2/ejemplo/Parcela.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/ejemplo/Parcela.kt
@@ -2,7 +2,7 @@ package ar.edu.unahur.obj2.ejemplo
 
 import kotlin.math.ceil
 
-open class Parcela(var ancho: Double, var largo: Double, val horasDeSol: Int, val listaDePlantas: MutableList<Planta>) {
+open class Parcela(var tipo:String= "generico", var ancho: Double, var largo: Double, val horasDeSol: Int, val listaDePlantas: MutableList<Planta>) {
     fun superficie() = ancho * largo
 
     fun cantMaxDePlantas() = ceil(if(ancho > largo) {superficie()/5} else {superficie()/3 + largo})
@@ -24,18 +24,24 @@ open class Parcela(var ancho: Double, var largo: Double, val horasDeSol: Int, va
 
     fun tieneComplicaciones() = listaDePlantas.any{ it.horasDeSolToleradas() > horasDeSol}
 
-    open fun seAsociaBienA(unaPlanta: Planta) = false
+    //open fun seAsociaBienA(unaPlanta: Planta) = false
+    fun cambiarATipo(unTipo: String)= unTipo.also { this.tipo = it }
+
+    fun seAsociaBien(unaPlanta: Planta) :Boolean {
+        var resultado:Boolean
+        if (tipo == "ecologica") {
+            resultado = !tieneComplicaciones() && unaPlanta.esIdeal(this)
+        }else if(tipo == "industrial") {
+            resultado = cantDePlantasEnParcela() <= 2 && unaPlanta.esFuerte()
+        }else{
+            error("en parcelas genericas no se analiza asociacion")
+        }
+        return resultado
+    }
 
     fun porcentajeBienAsociada(): Double {
         if(listaDePlantas.size  == 0) return 0.0
-        return listaDePlantas.count{n ->this.seAsociaBienA(n)} / listaDePlantas.size * 100.0
+        return listaDePlantas.count{n ->this.seAsociaBien(n)} / listaDePlantas.size * 100.0
     }
 
-}
-class ParcelaEcologica(ancho: Double, largo: Double, horasDeSol: Int, listaDePlantas: MutableList<Planta>) : Parcela(ancho, largo,horasDeSol,listaDePlantas){
-     override fun seAsociaBienA(unaPlanta: Planta) = !tieneComplicaciones() && unaPlanta.esIdeal(this)
-}
-
-class ParcelaIndustrial(ancho: Double, largo: Double, horasDeSol: Int, listaDePlantas: MutableList<Planta>) : Parcela(ancho, largo,horasDeSol,listaDePlantas){
-    override fun seAsociaBienA(unaPlanta: Planta) = listaDePlantas.size < 2 && unaPlanta.esFuerte()
 }
